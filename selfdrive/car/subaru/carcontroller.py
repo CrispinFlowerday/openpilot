d0f7f0cc13659776788b434909afb124b5c707f5
Crispin Flowerday <crispin@theflowerdays.com>
2021-04-21 17:35:21 -0700

    Add CAR

--- selfdrive/car/subaru/carcontroller.py
+++ selfdrive/car/subaru/carcontroller.py
@@ -1,6 +1,6 @@
 from selfdrive.car import apply_std_steer_torque_limits
 from selfdrive.car.subaru import subarucan
-from selfdrive.car.subaru.values import DBC, PREGLOBAL_CARS, REDUCED_TORQUE_CARS, CarControllerParams
+from selfdrive.car.subaru.values import DBC, CAR, PREGLOBAL_CARS, REDUCED_TORQUE_CARS, CarControllerParams
 from opendbc.can.packer import CANPacker
 
 
