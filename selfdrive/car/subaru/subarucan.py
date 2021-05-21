70173984f985dc30bcd376f3eedc9ff6f95b6747
Crispin Flowerday <crispin@theflowerdays.com>
2021-05-21 15:08:35 -0700

    Dirty hack to alter messages via paths

--- selfdrive/car/subaru/subarucan.py
+++ selfdrive/car/subaru/subarucan.py
@@ -41,14 +41,25 @@ def create_es_lkas(packer, es_lkas_msg, visual_alert, left_line, right_line, ena
       values["LKAS_Alert"] = 11
 
   # Test some values
-  values["LKAS_ACTIVE"] = 0 # Hypothesis, disable stock
-  values["LKAS_ENABLE_3"] = 1  # Hypothesis, enable display of left line
-  values["LKAS_ENABLE_2"] = 1  # Hypothesis, enable display of right line
-  values["Signal2"] = enabled ? 1 : 0 # 1 = white LKAS, 2 = green LKAS
+  values["LKAS_ACTIVE"] = os.path.exists("/tmp/lkas_active") ? 1 : 0 # Hypothesis, disable stock
+  values["LKAS_ENABLE_3"] = os.path.exists("/tmp/lkas_enable_3") ? 1 : 0  # Hypothesis, enable display of left line
+  values["LKAS_ENABLE_2"] = os.path.exists("/tmp/lkas_enable_2") ? 1 : 0  # Hypothesis, enable display of right line
+  if os.path.exists("/tmp/lkas_signal2_1"): # 1 = white LKAS, 2 = green LKAS
+    values["Signal2"] = 1 
+  elif os.path.exists("/tmp/lkas_signal2_2"):
+    values["Signal2"] = 2 
+  else:
+    values["Signal2"] = 0
 
-  values["LKAS_Left_Line_Visible"] = int(left_line)
-  values["LKAS_Right_Line_Visible"] = int(right_line)
+  values["LKAS_Left_Line_Visible"] = int(os.path.exists("/tmp/lkas_left_visible"))
+  values["LKAS_Right_Line_Visible"] = int(os.path.exists("/tmp/lkas_right_visible"))
 
+  values["LKAS_Left_Line_Light_Blink"] = int(os.path.exists("/tmp/lkas_left_blink"))
+  values["LKAS_Right_Line_Light_Blink"] = int(os.path.exists("/tmp/lkas_right_blink"))
+
+  values["LKAS_Left_Line_Green"] = int(os.path.exists("/tmp/lkas_left_green"))
+  values["LKAS_Right_Line_Green"] = int(os.path.exists("/tmp/lkas_right_green"))
+  
   return packer.make_can_msg("ES_LKAS_State", 0, values)
 
 def create_throttle(packer, throttle_msg, throttle_cmd):
