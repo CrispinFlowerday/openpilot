b677b2600df591f5346ce31ca8ca9994de9b03cc
Crispin Flowerday <crispin@theflowerdays.com>
2021-05-24 14:44:28 -0700

    Tweaks

--- selfdrive/car/subaru/subarucan.py
+++ selfdrive/car/subaru/subarucan.py
@@ -27,38 +27,39 @@ def create_es_distance(packer, es_distance_msg, pcm_cancel_cmd):
 
 def create_es_lkas(packer, es_lkas_msg, enabled, visual_alert, left_line, right_line, left_lane_depart, right_lane_depart):
 
-  if !enabled:
-    return es_lkas_msg
-
   values = copy.copy(es_lkas_msg)
 
-  # Clear out LKAS warnings (driver monitoring from stock)
-  if values["Keep_Hands_On_Wheel"] == 1:
-    values["Keep_Hands_On_Wheel"] = 0
+  # First, if we are enabled, clear out any existing warnings from Stock
+  if enabled:
+    if values["Keep_Hands_On_Wheel"] == 1:
+      values["Keep_Hands_On_Wheel"] = 0
 
-  # Clear LKAS disabled dash alert (from stock)
-  if values["LKAS_Alert"] in [ 27 ]:
-    values["LKAS_Alert"] = 0
+    # Clear LKAS disabled dash alert (from stock)
+    if values["LKAS_Alert"] in [ 27 ]:
+      values["LKAS_Alert"] = 0
 
-  # Setup dash display
-  values["LKAS_ACTIVE"] = 1 # Show correct display
-  values["LKAS_Dash_Icon"] = 2 # Green enabled icon
-  values["LKAS_Left_Line_Enable"] = 1 # Allow showing left line
-  values["LKAS_Right_Line_Enable"] = 1 # Allow showing right line
-  values["LKAS_Left_Line_Visible"] = int(left_line)
-  values["LKAS_Right_Line_Visible"] = int(right_line)
 
-  # Show warnings as appropriate
+  # Now show any OP required warnings
   if visual_alert == VisualAlert.steerRequired:
     values["Keep_Hands_On_Wheel"] = 1
   
-  # Show correct Left/Right display
   if visual_alert == VisualAlert.ldw and values["LKAS_Alert"] == 0:
     if left_lane_depart:
       values["LKAS_Alert"] = 12 # Left lane departure dash alert
 
     elif right_lane_depart:
       values["LKAS_Alert"] = 11 # Right lane departure dash alert
+
+
+  # If we are enabled, then show out LKAS display
+  if enabled:
+    # Setup dash display
+    values["LKAS_ACTIVE"] = 1 # Show correct display
+    values["LKAS_Dash_Icon"] = 2 # Green enabled icon
+    values["LKAS_Left_Line_Enable"] = 1 # Allow showing left line
+    values["LKAS_Right_Line_Enable"] = 1 # Allow showing right line
+    values["LKAS_Left_Line_Visible"] = int(left_line)
+    values["LKAS_Right_Line_Visible"] = int(right_line)
 
   # Signal2=
   # LKAS_ACTIVE=
